5e2e6565f740e9bdde3d9d61c24c1b4b6dfe1824
QuangAnh29 <dinhquang02092000@gmail.com>
2023-09-28 18:51:11 +0700

    Commit 1

---
+++ Assets/Scripts/InteractionManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionManager : MonoBehaviour
+{
+    public static InteractionManager Instance { get; set; }
+
+    public Weapon hoveredWeapon = null;
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+
+
+    private void Update()
+    {
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+        if (Physics.Raycast(ray,out hit))
+        {
+            GameObject objectHitByRayCat = hit.transform.gameObject;
+
+            if (objectHitByRayCat.GetComponent<Weapon>() && objectHitByRayCat.GetComponent<Weapon>().isActiveWeapon == false)
+            {
+                hoveredWeapon = objectHitByRayCat.gameObject.GetComponent<Weapon>();
+                hoveredWeapon.GetComponent<Outline>().enabled = true;
+
+                if (Input.GetKeyDown(KeyCode.F))
+                {
+                    WeaponManager.Instance.PickupWeapon(objectHitByRayCat.gameObject);
+                }
+            }
+            else
+            {
+                if (hoveredWeapon)
+                {
+                    hoveredWeapon.GetComponent<Outline>().enabled = false;
+                }
+            }
+        }
+    }
+}
